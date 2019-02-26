eb45a262276fe3dd6725cda48fdc295cceffe4ba
Joe Gasiorek <joe.gasiorek@gmail.com>
2019-02-25 21:17:40 -0800

    crypto

---
+++ Send/Common/Keychain.swift
@@ -0,0 +1,119 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import CryptoSwift
+
+class Keychain {
+    var nonce: String
+    var iv: [UInt8]
+    var rawSecret: [UInt8]
+
+    var encryptKey: [UInt8]?
+    var metadataKey: [UInt8]?
+    var authKey: [UInt8]?
+
+    convenience init() {
+        self.init(secretKeyB64: "ECogp0rQtYjGgKqBfwxw3w==", nonce: "yRCdyQ1EMSA3mo4rqSkuNQ==", ivB64: nil)
+    }
+
+    init(secretKeyB64: String?,
+         nonce: String?,
+         ivB64: String?) {
+
+        self.nonce = nonce ?? "yRCdyQ1EMSA3mo4rqSkuNQ=="
+        self.iv = ivB64?.bytes ?? AES.randomIV(12)
+        self.rawSecret = Data(base64Encoded: secretKeyB64 ?? "", options: .init(rawValue: 0))?.bytes ?? AES.randomIV(16)
+
+        do {
+            self.encryptKey = try HKDF(password: self.rawSecret,
+                               salt: Array<UInt8>(),
+                               info: "encryption".bytes,
+                               keyLength: 16,
+                               variant: HMAC.Variant.sha256).calculate()
+
+            self.metadataKey = try HKDF(password: self.rawSecret,
+                                   salt: Array<UInt8>(),
+                                   info: "metadata".bytes,
+                                   keyLength: 16,
+                                   variant: HMAC.Variant.sha256).calculate()
+
+            self.authKey = try HKDF(password: self.rawSecret,
+                                   salt: Array<UInt8>(),
+                                   info: "authentication".bytes,
+                                   keyLength: 64,
+                                   variant: HMAC.Variant.sha256).calculate()
+
+        } catch let error {
+            debugPrint(error)
+        }
+    }
+
+    func authHeader() -> String? {
+        if let a = self.auth() {
+            return "send-v1 \(a)"
+        }
+
+        return nil
+    }
+
+    func auth() -> String? {
+        guard let authKey = self.authKey else { return nil }
+
+        do {
+            let hmac = HMAC(key: authKey, variant: .sha256)
+            if let nonceBytes = Data(base64Encoded: self.nonce , options: .init(rawValue: 0))?.bytes {
+                let sig = try hmac.authenticate(nonceBytes)
+                return self.toBase64(sig)
+            }
+        } catch let error {
+            debugPrint(error)
+        }
+
+        return nil
+    }
+
+    func authKeyB64() -> String? {
+        guard let authKey = self.authKey else { return nil }
+        return self.toBase64(authKey)
+    }
+
+    func encryptFile(plaintext: Data) -> Data? {
+        guard let encryptKey = self.encryptKey else { return nil }
+
+        do {
+            let aes = try AES(key: encryptKey, blockMode: GCM(iv: self.iv, additionalAuthenticatedData: nil, tagLength: 128, mode: .combined))
+            let ciphertext = try aes.encrypt(plaintext.bytes)
+            return Data(bytes: ciphertext)
+        } catch let error {
+            debugPrint(error)
+        }
+
+        return nil
+    }
+
+    func encryptMetadata(name: String, type: String?) -> String? {
+        guard let metadataKey = self.metadataKey else { return nil }
+
+        let dataType = type ?? "application/octet-stream"
+
+        guard let ivString = self.toBase64(self.iv) else { return nil }
+
+        do {
+            let aes = try AES(key: metadataKey, blockMode: GCM(iv: Array<UInt8>(repeating: 0, count: 12), additionalAuthenticatedData: nil, tagLength: 128, mode: .combined))
+            let ciphertext = try aes.encrypt("{iv:'\(ivString)',name:'\(name)',type:'\(dataType)'}".bytes)
+            return self.toBase64(ciphertext)
+        } catch let error {
+            debugPrint(error)
+        }
+
+        return nil
+    }
+
+    func toBase64(_ data: [UInt8]) -> String? {
+        return data.toBase64()?
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "/", with: "_")
+            .replacingOccurrences(of: "=", with: "")
+    }
+}
--- Send/Store/UploadStore.swift
+++ Send/Store/UploadStore.swift
@@ -41,16 +41,60 @@ class UploadStore {
             .subscribe(onNext: { (action, files) in
                 for file in files {
                     do {
+                        let keychain = Keychain()
                         let data = try Data(contentsOf: file.url)
-                        AF.upload(data, to: "https://send.firefox.com/api/upload")
+                        guard let encryptedFile = keychain.encryptFile(plaintext: data) else { continue }
+                        guard let metadata = keychain.encryptMetadata(name: file.name, type: file.type) else { continue }
+                        guard let authHeader = keychain.authKeyB64() else { continue }
+                        guard let rawSecret = keychain.toBase64(keychain.rawSecret) else { continue }
+                        let headers: HTTPHeaders = [
+                            "X-File-Metadata": metadata,
+                            "Authorization": "send-v1 \(authHeader)",
+                        ]
+
+                        AF.upload(multipartFormData: { multipartFormData in
+                            multipartFormData.append(encryptedFile, withName: "data", mimeType: "application/octet-stream")
+                            },
+//                                  to: "https://send.firefox.com/api/upload",
+                            to: "http://0.0.0.0:1443/api/upload",
+                                  method: .post,
+                                  headers: headers)
                             .uploadProgress { progress in // main queue by default
                                 let p = Float((progress.completedUnitCount / progress.totalUnitCount) * 100)
                                 print(p)
                                 self._progress.onNext(p)
                                 print("Upload Progress: \(progress.fractionCompleted)")
-                            }.responseString(completionHandler: { (response) in
+
+                            }.responseJSON(completionHandler: { (response) in
+                                if let json = response.result.value as? NSDictionary {
+                                    debugPrint(json)
+                                    if let url = json.object(forKey: "url") as? String {
+                                        debugPrint("\(url)#\(rawSecret)")
+                                    }
+
+                                    if let newNonce = response.response?.httpHeaders["WWW-Authenticate"]?.split(separator: " ")[1] {
+                                        keychain.nonce = String(newNonce)
+                                    }
+
+                                    if let ownerToken = json.object(forKey: "owner") as? String {
+                                        if let id = json.object(forKey: "id") as? String {
+                                        let params: Parameters = [
+                                            "owner_token": ownerToken
+                                        ]
+
+                                        AF.request("http://0.0.0.0:1443/api/info/\(id)",
+                                                   method: .post,
+                                                   parameters: params,
+                                                   encoding: JSONEncoding.default)
+                                            .responseJSON(completionHandler: { (response) in
+                                               // debugPrint(response)
+                                            })
+                                        }
+                                    }
+                                }
+                                debugPrint(response)
                                 self._progress.onNext(1.0)
-                            debugPrint(response)
+
                         })
                     } catch let error {
                         debugPrint(error)
